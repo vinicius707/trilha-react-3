e59d7d0df1894af1e62b5e0a7e343fb85ae5f9a2
“Vinicius <viniciussilva707@gmail.com>
2025-12-06 13:21:03 -0300

    Refactor App, Filtro, and ListaDeEventos components to streamline event filtering logic and integrate Recoil state management. Introduce IFiltroDeEventos interface and update atom for better state handling.

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,11 @@
-import { useState } from "react";
 import style from "./App.module.scss";
 import Card from "./components/Card";
 import Formulario from "./components/Formulario";
-import { IEvento } from "./interfaces/IEvento";
 import Calendario from "./components/Calendario";
 import ListaDeEventos from "./components/ListaDeEventos";
 import { RecoilRoot } from "recoil";
 
 function App() {
-  const [filtro, setFiltro] = useState<Date | null>();
-
-  const adicionarEvento = (evento: IEvento) => {
-    evento.id = Math.round(new Date().getTime() / 1000);
-  };
-
-  const aplicarFiltro = (data: Date | null) => {
-    setFiltro(data);
-  };
-
   return (
     <RecoilRoot>
       <div className={style.App}>
@@ -27,7 +15,7 @@ function App() {
           </Card>
           <hr />
           <Card>
-            <ListaDeEventos aoFiltroAplicado={aplicarFiltro} />
+            <ListaDeEventos />
           </Card>
         </div>
         <div className={style.Coluna}>

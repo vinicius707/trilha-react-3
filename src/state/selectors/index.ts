acfd559d8a880a2a2f32ff8331a3efb9d9e58c01
“Vinicius <viniciussilva707@gmail.com>
2025-12-06 13:56:16 -0300

    Refactor App component to utilize Suspense for loading state and update listaDeEventosState to initialize with async data from eventosAsync selector. This enhances user experience during data fetching and improves state management.

--- src/state/selectors/index.ts
+++ src/state/selectors/index.ts
@@ -1,5 +1,6 @@
 import { selector } from "recoil";
 import { filtroDeEventos, listaDeEventosState } from "../atom";
+import { IEvento } from "../../interfaces/IEvento";
 
 export const eventosFiltradosState = selector({
   key: "eventosFiltradosState",
@@ -16,5 +17,18 @@ export const eventosFiltradosState = selector({
       return mesmoDia;
     });
     return eventos;
+  },
+});
+
+export const eventosAsync = selector({
+  key: "eventosAsync",
+  get: async () => {
+    const respostaHttp = await fetch("http://localhost:8080/eventos");
+    const eventosJson: IEvento[] = await respostaHttp.json();
+    return eventosJson.map((evento) => ({
+      ...evento,
+      inicio: new Date(evento.inicio),
+      fim: new Date(evento.fim),
+    }));
   },
 });

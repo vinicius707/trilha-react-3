b50a5ee018c16eb2956298061f3d0470c7001085
“Vinicius <viniciussilva707@gmail.com>
2025-12-06 14:08:05 -0300

    Add DebugObserver component for Recoil state debugging and update tsconfig.json for downlevelIteration support

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,12 @@ import Calendario from "./components/Calendario";
 import ListaDeEventos from "./components/ListaDeEventos";
 import { RecoilRoot } from "recoil";
 import { Suspense } from "react";
+import DebugObserver from "./components/DebugObserver";
 
 function App() {
   return (
     <RecoilRoot>
+      <DebugObserver></DebugObserver>
       <Suspense fallback="Está carregando">
         <div className={style.App}>
           <div className={style.Coluna}>
